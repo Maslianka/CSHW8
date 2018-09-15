4d6a5f22c3dba0ab9b211039e8e9cacba0ff40d1
Nazar <nazar2131@ukr.net>
2018-09-15 20:55:00 +0300

    one more commit

---
+++ CSHW8P2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSHW8P2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List <string> lines = new List<string>();
+            using (StreamReader streamReader = new StreamReader("text.txt"))
+            {
+                string line;
+               
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    lines.Add(line);
+                }
+            }
+            foreach(string s in lines)
+            {
+                Console.WriteLine(s);
+            }
+
+            for (int i = 0; i < lines.Count; i++)
+            {
+                Console.WriteLine((i + 1) + "line has " + lines[i].Length + " characters");
+            }
+        
+            Console.ReadKey();
+        }
+    }
+}
